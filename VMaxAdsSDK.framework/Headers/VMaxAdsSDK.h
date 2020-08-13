b31a9a44c50fe7a07d2d521b4b28e236119d5550
Cloy Monis <cloymonis@Cloys-MacBook-Air.local>
2020-08-13 13:30:12 +0530

    framework 0.0.5

---
+++ VMaxAdsSDK.framework/Headers/VMaxAdsSDK.h
@@ -0,0 +1,19 @@
+//
+//  VMaxAdsSDK.h
+//  VMaxAdsSDK
+//
+//  Created by Cloy Monis on 12/08/20.
+//  Copyright © 2020 Cloy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VMaxAdsSDK.
+FOUNDATION_EXPORT double VMaxAdsSDKVersionNumber;
+
+//! Project version string for VMaxAdsSDK.
+FOUNDATION_EXPORT const unsigned char VMaxAdsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VMaxAdsSDK/PublicHeader.h>
+#import "VMaxAdView.h"
+
