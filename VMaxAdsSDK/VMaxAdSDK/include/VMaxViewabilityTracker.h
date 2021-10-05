14a3f77904d79037bba78e2f37b35486acd0330b
CloyMonis <cloy.m@vserv.com>
2021-10-05 10:47:02 +0530

    Intergtation SDK test

--- VMaxAdsSDK/VMaxAdSDK/include/VMaxViewabilityTracker.h
+++
@@ -1,9 +0,0 @@
-//
-//  VMaxViewabilityTracker.h
-
-
-#import <Foundation/Foundation.h>
-
-@interface VMaxViewabilityTracker : NSObject
--(void) dispatchEvent:(id)IMAAdEvent adLayer:(CALayer *)adCaLayer IMAAdManager :(id)adsManager;
-@end
